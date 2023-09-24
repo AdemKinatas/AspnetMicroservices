35951b0a4211a77c37da5db6cade1fb3fd831203
Adem Kınataş <ademkinatas@gmail.com>
2023-09-25 00:40:20 +0300

    added data and repository layers and created catalog api

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private DatabaseSettings _databaseSettings;
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            _databaseSettings = configuration.GetSection("DatabaseSettings").Get<DatabaseSettings>();
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+
+            Products = database.GetCollection<Product>(_databaseSettings.CollectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
