dd32d6e887ac610a318d288564297378cfb6b50d
Adem Kınataş <ademkinatas@gmail.com>
2023-10-09 23:53:25 +0300

    added entities to basket api

---
+++ src/Services/Basket/Basket.API/Entities/Cart.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.API.Entities
+{
+    public class Cart
+    {
+        public string? UserName { get; set; }
+        public List<CartItem> CartItems { get; set; }  = new ();
+
+        public Cart() { }
+
+        public Cart(string userName)
+        {
+            userName = userName ?? string.Empty;
+        }
+
+        public decimal TotalPrice 
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (var item in CartItems)
+                {
+                    total += item.Price * item.Quantity;
+                }
+                return total;
+            }
+        }
+    }
+}
